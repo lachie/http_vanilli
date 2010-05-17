7c29e2a697a9e240f93ceacddc05cb3fe31c05e8
lachie <lachiec@gmail.com>
2010-05-17 13:21:29 +1000

    request impl

--- lib/http_vanilli/net_http/request.rb
+++ lib/http_vanilli/net_http/request.rb
@@ -3,7 +3,27 @@ require 'http_vanilli/request'
 module HttpVanilli
   module NetHttp
     class Request < HttpVanilli::Request
+      attr_reader :block, :uri
+
       def initialize(http,request,body=nil,&block)
+        @http             = http
+        @original_request = request
+        @block            = block
+        @body             = body
+
+        protocol = http.use_ssl? ? "https" : "http"
+
+        path = request.path
+        path = Addressable::URI.parse(request.path).request_uri if request.path =~ /^https?:/
+
+        @uri    = Addressable::URI.parse( "#{protocol}://#{http.address}:#{http.port}#{path}" )
+        @method = request.method.downcase.to_sym
+      end
+
+      def host; @uri.host end
+
+      def to_s
+        "#{@method} #{@uri.to_s}"
       end
     end
   end
--- lib/http_vanilli/request.rb
+++ lib/http_vanilli/request.rb
@@ -1,4 +1,12 @@
 module HttpVanilli
   class Request
+    def self.build(kind,*args,&block)
+      case kind
+      when :net_http
+        HttpVanilli::NetHttp::Request.new(*args,&block)
+      else
+        raise "Unknown request kind #{kind}"
+      end
+    end
   end
 end
