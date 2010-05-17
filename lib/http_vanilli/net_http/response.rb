db3ff224fec0c78e02551fce7c3e0c2191e3d37b
lachie <lachiec@gmail.com>
2010-05-17 13:21:43 +1000

    response impl

--- lib/http_vanilli/net_http/response.rb
+++ lib/http_vanilli/net_http/response.rb
@@ -1,13 +1,17 @@
 module HttpVanilli
   module NetHttp
-    class Response
-      attr_accessor :body, :headers
+    class Response < HttpVanilli::Response
+      attr_accessor :headers, :body
       attr_reader :code, :message
-      def initialize(code,message)
-        @code,@message = code,message
-        @headers = {}
+
+      def initialize(code,headers,body,message=StatusMessage[code.to_i])
+        @code,@body,@message = code.to_i,body,message
+        @headers = headers || {}
       end
 
+      def body
+        @body.join("")
+      end
 
       def to_net_http
         response = Net::HTTPResponse.send(:response_class, code.to_s).new("1.0", code.to_s, message)
